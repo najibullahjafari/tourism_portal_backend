7ab6cea94d65b236b44bba092aeac691baca0704
= <=>
2024-05-02 23:07:34 +0430

    Add sidbar

---
+++ resources/js/components/Pages/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Sidebar = () => {
+  return (
+    <div>
+      <h1>Hello Sidebar!</h1>
+      {/* the sidbare components about tourism portal */}
+    </div>
+  );
+};
+export default Sidebar;
