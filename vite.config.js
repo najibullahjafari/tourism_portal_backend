b31196264f1d601b8d90323582502a0e6e7202ec
= <=>
2024-05-02 23:08:53 +0430

    Install react dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
-import "dotenv/config";
 
 export default defineConfig({
-    build: {
-        minify: process.env.APP_ENV === "production" ? "esbuild" : false,
-        cssMinify: process.env.APP_ENV === "production",
-    },
     plugins: [
         laravel({
-            input: ["resources/react/app.tsx"], //enty point for react app
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
         react(),
