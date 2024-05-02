7a8706f4e6a09ccd2019a4b0aa40be0b014c2efb
= <=>
2024-05-02 23:07:01 +0430

    Add react load file

---
+++ resources/js/components/App.jsx
@@ -0,0 +1,13 @@
+import Dashboard from "./Pages/Dashboard/Dashboard";
+
+function App() {
+    return (
+        <div className="App">
+            <div>
+                <Dashboard />
+            </div>
+        </div>
+    );
+}
+
+export default App;
