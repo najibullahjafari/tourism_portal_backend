5fb50d83782038f56af8e46be113f85d88980ed2
= <=>
2024-05-02 23:08:35 +0430

    Add app

---
+++ resources/js/Frontend/app.jsx
@@ -0,0 +1,11 @@
+// here the dashboard
+import react from "react";
+
+const dashboard = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+    );
+};
+export default dashboard;
