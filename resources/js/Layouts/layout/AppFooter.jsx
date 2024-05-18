bc297408065a0799e06c6f498bcee54d1ea0139d
heyday56 <mohammadbasirpayenda@gmail.com>
2024-05-18 09:14:33 -0700

    Change Footer

--- resources/js/Layouts/layout/AppFooter.jsx
+++ resources/js/Layouts/layout/AppFooter.jsx
@@ -1,10 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
 const AppFooter = () => {
     return (
         <div className="layout-footer">
-            <span>Made with <span className="text-red-500">&#10084;&#65039;</span> by</span>
-            <span className="font-medium ml-2">boadusamuel & team</span>
+            <span>Afghanistan Portal Tourism</span>
         </div>
     );
 };
