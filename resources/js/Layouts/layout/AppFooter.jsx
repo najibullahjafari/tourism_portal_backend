6d707bad456fb88ea0cf5da769085968549cf951
= <=>
2024-05-28 09:05:56 +0430

    Update footer

--- resources/js/Layouts/layout/AppFooter.jsx
+++ resources/js/Layouts/layout/AppFooter.jsx
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
 const AppFooter = () => {
-    return (
-        <div className="layout-footer">
-            <span>Made with <span className="text-red-500">&#10084;&#65039;</span> by</span>
-            <span className="font-medium ml-2">boadusamuel & team</span>
-        </div>
-    );
+  return (
+    <div className="layout-footer">
+      <span>
+        Made with <span className="text-red-500">&#10084;&#65039;</span> by
+      </span>
+      <span className="font-medium ml-2">Najibullah & Mohammad Bassir</span>
+    </div>
+  );
 };
 
 export default AppFooter;
