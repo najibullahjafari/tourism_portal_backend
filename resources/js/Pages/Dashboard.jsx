85e1334929149db04e452b05ea1002b77c815536
= <=>
2024-05-30 20:55:38 +0430

    UPdate Dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,31 +1,32 @@
-import {Button} from 'primereact/button';
-import {Chart} from 'primereact/chart';
-import {Menu} from 'primereact/menu';
-import React, {useContext, useEffect, useRef, useState} from 'react';
-import {LayoutContext} from '@/Layouts/layout/context/layoutcontext';
+import { Button } from "primereact/button";
+import { Chart } from "primereact/chart";
+import { Menu } from "primereact/menu";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { LayoutContext } from "@/Layouts/layout/context/layoutcontext";
 import Layout from "@/Layouts/layout/layout.jsx";
 import DashboardInfoCard from "@/Components/DashboardInfoCard.jsx";
+import { router } from "@inertiajs/react";
 
 const lineData = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
         {
-            label: 'First Dataset',
+            label: "Interanl Tourists",
             data: [65, 59, 80, 81, 56, 55, 40],
             fill: false,
-            backgroundColor: '#2f4860',
-            borderColor: '#2f4860',
-            tension: 0.4
+            backgroundColor: "#2f4860",
+            borderColor: "#2f4860",
+            tension: 0.4,
         },
         {
-            label: 'Second Dataset',
+            label: "External Tourists",
             data: [28, 48, 40, 19, 86, 27, 90],
             fill: false,
-            backgroundColor: '#00bb7e',
-            borderColor: '#00bb7e',
-            tension: 0.4
-        }
-    ]
+            backgroundColor: "#00bb7e",
+            borderColor: "#00bb7e",
+            tension: 0.4,
+        },
+    ],
 };
 
 const Dashboard = () => {
@@ -33,35 +34,36 @@ const Dashboard = () => {
     const menu1 = useRef(null);
     const menu2 = useRef(null);
     const [lineOptions, setLineOptions] = useState({});
-    const {layoutConfig} = useContext(LayoutContext);
+    const { layoutConfig } = useContext(LayoutContext);
+    const [lastTransportationId, setLastTransportationId] = useState(null);
 
     const applyLightTheme = () => {
         const lineOptions = {
             plugins: {
                 legend: {
                     labels: {
-                        color: '#495057'
-                    }
-                }
+                        color: "#495057",
+                    },
+                },
             },
             scales: {
                 x: {
                     ticks: {
-                        color: '#495057'
+                        color: "#495057",
                     },
                     grid: {
-                        color: '#ebedef'
-                    }
+                        color: "#ebedef",
+                    },
                 },
                 y: {
                     ticks: {
-                        color: '#495057'
+                        color: "#495057",
                     },
                     grid: {
-                        color: '#ebedef'
-                    }
-                }
-            }
+                        color: "#ebedef",
+                    },
+                },
+            },
         };
 
         setLineOptions(lineOptions);
@@ -72,35 +74,39 @@ const Dashboard = () => {
             plugins: {
                 legend: {
                     labels: {
-                        color: '#ebedef'
-                    }
-                }
+                        color: "#ebedef",
+                    },
+                },
             },
             scales: {
                 x: {
                     ticks: {
-                        color: '#ebedef'
+                        color: "#ebedef",
                     },
                     grid: {
-                        color: 'rgba(160, 167, 181, .3)'
-                    }
+                        color: "rgba(160, 167, 181, .3)",
+                    },
                 },
                 y: {
                     ticks: {
-                        color: '#ebedef'
+                        color: "#ebedef",
                     },
                     grid: {
-                        color: 'rgba(160, 167, 181, .3)'
-                    }
-                }
-            }
+                        color: "rgba(160, 167, 181, .3)",
+                    },
+                },
+            },
         };
 
         setLineOptions(lineOptions);
     };
 
+    const getLastTransportationId = () => {
+        router.get("/car/last-transportation-id");
+    };
+
     useEffect(() => {
-        if (layoutConfig.colorScheme === 'light') {
+        if (layoutConfig.colorScheme === "light") {
             applyLightTheme();
         } else {
             applyDarkTheme();
@@ -110,53 +116,77 @@ const Dashboard = () => {
     return (
         <Layout>
             <div className="grid">
-                <DashboardInfoCard title="Orders"
-                                   value="152"
-                                   icon="map-marker"
-                                   iconColor="blue"
-                                   descriptionValue="24 new"
-                                   descriptionText="since last visit">
-                </DashboardInfoCard>
-                <DashboardInfoCard title="Revenue"
-                                   value="GHS 2.100"
-                                   icon="map-marker"
-                                   iconColor="orange"
-                                   descriptionValue="%52+"
-                                   descriptionText="since last week">
-                </DashboardInfoCard>
-                <DashboardInfoCard title="Customers" value="28441"
-                                   descriptionValue="520"
-                                   icon="inbox"
-                                   iconColor="cyan"
-                                   descriptionText="since last week">
-                </DashboardInfoCard>
-                <DashboardInfoCard title="Comments" value="152 Unread"
-                                   descriptionValue="85"
-                                   icon="comment"
-                                   iconColor="purple"
-                                   descriptionText="responded">
-                </DashboardInfoCard>
+                <DashboardInfoCard
+                    title="Requested Cars"
+                    value="25"
+                    icon="map-marker"
+                    iconColor="blue"
+                    descriptionValue="24 new"
+                    descriptionText="since established"
+                ></DashboardInfoCard>
+                <DashboardInfoCard
+                    title="Revenue"
+                    value="AF 2.100"
+                    icon="map-marker"
+                    iconColor="orange"
+                    descriptionValue="%52+"
+                    descriptionText="since last week"
+                ></DashboardInfoCard>
+                <DashboardInfoCard
+                    title="Users"
+                    value="28441"
+                    descriptionValue="520"
+                    icon="inbox"
+                    iconColor="cyan"
+                    descriptionText="since last week"
+                ></DashboardInfoCard>
+                <DashboardInfoCard
+                    title="Comments"
+                    value="152 Unread"
+                    descriptionValue="85"
+                    icon="comment"
+                    iconColor="purple"
+                    descriptionText="responded"
+                ></DashboardInfoCard>
 
                 <div className="col-12 xl:col-6">
                     <div className="card">
-                        <h5>Sales Overview</h5>
-                        <Chart type="line" data={lineData} options={lineOptions}/>
+                        <h5> Overview</h5>
+                        <Chart
+                            type="line"
+                            data={lineData}
+                            options={lineOptions}
+                        />
                     </div>
                 </div>
 
                 <div className="col-12 xl:col-6">
                     <div className="card">
                         <div className="flex justify-content-between align-items-center mb-5">
-                            <h5>Best Selling Products</h5>
+                            <h5>Most visited places</h5>
                             <div>
-                                <Button type="button" icon="pi pi-ellipsis-v" rounded text className="p-button-plain"
-                                        onClick={(event) => menu1.current?.toggle(event)}/>
+                                <Button
+                                    type="button"
+                                    icon="pi pi-ellipsis-v"
+                                    rounded
+                                    text
+                                    className="p-button-plain"
+                                    onClick={(event) =>
+                                        menu1.current?.toggle(event)
+                                    }
+                                />
                                 <Menu
                                     ref={menu1}
                                     popup
                                     model={[
-                                        {label: 'Add New', icon: 'pi pi-fw pi-plus'},
-                                        {label: 'Remove', icon: 'pi pi-fw pi-minus'}
+                                        {
+                                            label: "Add New",
+                                            icon: "pi pi-fw pi-plus",
+                                        },
+                                        {
+                                            label: "Remove",
+                                            icon: "pi pi-fw pi-minus",
+                                        },
                                     ]}
                                 />
                             </div>
@@ -164,54 +194,99 @@ const Dashboard = () => {
                         <ul className="list-none p-0 m-0">
                             <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
                                 <div>
-                                    <span className="text-900 font-medium mr-2 mb-1 md:mb-0">Space T-Shirt</span>
-                                    <div className="mt-1 text-600">Clothing</div>
+                                    <span className="text-900 font-medium mr-2 mb-1 md:mb-0">
+                                        Bamyan
+                                    </span>
+                                    <div className="mt-1 text-600">
+                                        Band Amir
+                                    </div>
                                 </div>
                                 <div className="mt-2 md:mt-0 flex align-items-center">
-                                    <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
-                                         style={{height: '8px'}}>
-                                        <div className="bg-orange-500 h-full" style={{width: '50%'}}/>
+                                    <div
+                                        className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
+                                        style={{ height: "8px" }}
+                                    >
+                                        <div
+                                            className="bg-orange-500 h-full"
+                                            style={{ width: "50%" }}
+                                        />
                                     </div>
-                                    <span className="text-orange-500 ml-3 font-medium">%50</span>
+                                    <span className="text-orange-500 ml-3 font-medium">
+                                        %50
+                                    </span>
                                 </div>
                             </li>
                             <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
                                 <div>
-                                    <span className="text-900 font-medium mr-2 mb-1 md:mb-0">Portal Sticker</span>
-                                    <div className="mt-1 text-600">Accessories</div>
+                                    <span className="text-900 font-medium mr-2 mb-1 md:mb-0">
+                                        Mazar Sharif
+                                    </span>
+                                    <div className="mt-1 text-600">
+                                        Band Qush Tepa
+                                    </div>
                                 </div>
                                 <div className="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-                                    <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
-                                         style={{height: '8px'}}>
-                                        <div className="bg-cyan-500 h-full" style={{width: '16%'}}/>
+                                    <div
+                                        className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
+                                        style={{ height: "8px" }}
+                                    >
+                                        <div
+                                            className="bg-cyan-500 h-full"
+                                            style={{ width: "16%" }}
+                                        />
                                     </div>
-                                    <span className="text-cyan-500 ml-3 font-medium">%16</span>
+                                    <span className="text-cyan-500 ml-3 font-medium">
+                                        %16
+                                    </span>
                                 </div>
                             </li>
                             <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
                                 <div>
-                                    <span className="text-900 font-medium mr-2 mb-1 md:mb-0">Supernova Sticker</span>
-                                    <div className="mt-1 text-600">Accessories</div>
+                                    <span className="text-900 font-medium mr-2 mb-1 md:mb-0">
+                                        Noristan
+                                    </span>
+                                    <div className="mt-1 text-600">
+                                        {/* name of a nice place of Nooristan province of aghanistan */}
+                                        Band Qala
+                                    </div>
                                 </div>
                                 <div className="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-                                    <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
-                                         style={{height: '8px'}}>
-                                        <div className="bg-pink-500 h-full" style={{width: '67%'}}/>
+                                    <div
+                                        className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
+                                        style={{ height: "8px" }}
+                                    >
+                                        <div
+                                            className="bg-pink-500 h-full"
+                                            style={{ width: "67%" }}
+                                        />
                                     </div>
-                                    <span className="text-pink-500 ml-3 font-medium">%67</span>
+                                    <span className="text-pink-500 ml-3 font-medium">
+                                        %67
+                                    </span>
                                 </div>
                             </li>
                             <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
                                 <div>
-                                    <span className="text-900 font-medium mr-2 mb-1 md:mb-0">Wonders Notebook</span>
-                                    <div className="mt-1 text-600">Office</div>
+                                    <span className="text-900 font-medium mr-2 mb-1 md:mb-0">
+                                        Jalalabad
+                                    </span>
+                                    <div className="mt-1 text-600">
+                                        Dare Noor
+                                    </div>
                                 </div>
                                 <div className="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-                                    <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
-                                         style={{height: '8px'}}>
-                                        <div className="bg-green-500 h-full" style={{width: '35%'}}/>
+                                    <div
+                                        className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem"
+                                        style={{ height: "8px" }}
+                                    >
+                                        <div
+                                            className="bg-green-500 h-full"
+                                            style={{ width: "35%" }}
+                                        />
                                     </div>
-                                    <span className="text-green-500 ml-3 font-medium">%35</span>
+                                    <span className="text-green-500 ml-3 font-medium">
+                                        %35
+                                    </span>
                                 </div>
                             </li>
                         </ul>
@@ -223,4 +298,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
