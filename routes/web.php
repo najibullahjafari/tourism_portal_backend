e35d310792bc5ff6433ad923ac6ee1d1a6c28222
= <=>
2024-05-30 21:00:32 +0430

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -36,13 +36,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // transportation part
     Route::post('car/requests', [TransportationController::class, 'store'])->name('transportation.requests');
     Route::delete('car/requests/{id}', [TransportationController::class, 'destroy'])->name('transportation.destroy');
-    Route::post('car/requests', [TransportationController::class, 'store'])->name('transportation.requests');
+    Route::post('car/requests/', [TransportationController::class, 'store'])->name('transportation.requests');
+    Route::post('car/accepted/{id}', [TransportationController::class, 'acceptCar']);
+    Route::get('/car/last-transportation-id', [TransportationController::class, 'lastTransportationId']);
+    Route::post('car/rejected/{id}', [TransportationController::class, 'rejectCar']);
     Route::get('/cars', [TransportationController::class, 'index'])->name('cars');
+    Route::get('cars/requested/cars', [TransportationController::class, 'showrequests'])->name('cars.requested.cars');
     Route::get('/cars/requests', function () {
         return Inertia::render('Transportation/CarRequests');
-    })->name('cars/requests');
+    })->name('cars.requests');
 });
 
 
