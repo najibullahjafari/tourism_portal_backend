efc4a70792318c48ac6850effd7676c479388452
= <=>
2024-06-11 09:44:41 +0430

    Update car controller

--- resources/js/Pages/Transportation/CarRequests.jsx
+++ resources/js/Pages/Transportation/CarRequests.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Head, useForm } from "@inertiajs/react";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
@@ -6,6 +6,8 @@ import Layout from "@/Layouts/layout/layout.jsx";
 import InputError from "@/Components/InputError";
 import { InputTextarea } from "primereact/inputtextarea";
 import { FloatLabel } from "primereact/floatlabel";
+import { Toast } from "primereact/toast";
+import { FileUpload } from "primereact/fileupload";
 
 export default function Transportation() {
     const { data, setData, post, processing, errors, reset } = useForm({
@@ -33,12 +35,24 @@ export default function Transportation() {
         reset(data);
     };
 
+    // for the file upload
+    const toast = useRef(null);
+
+    const onUpload = () => {
+        console.log("File Uploaded");
+        toast.current.show({
+            severity: "info",
+            summary: "Success",
+            detail: "File Uploaded",
+        });
+    };
+
     // const handleFileUpload = (e) => {
     //     const file = e.target.files[0];
     //     setData("passport", file);
     // };
     const handleFileUpload = (event) => {
-        const file = event.target.files[0];
+        const file = event.files[0];
         setData("passport", file);
         if (file) {
             const reader = new FileReader();
@@ -60,7 +74,7 @@ export default function Transportation() {
                         </div>
                     </div>
                     <form onSubmit={submit}>
-                        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 ">
+                        <div className="grid grid-cols-3 md:grid-cols-3 gap-4 ">
                             <div className="mb-3">
                                 <label
                                     htmlFor="name"
@@ -72,7 +86,7 @@ export default function Transportation() {
                                     id="name"
                                     type="text"
                                     placeholder="Name"
-                                    className="w-full"
+                                    className="w-full rounded-md border border-gray-400 "
                                     value={data.name}
                                     onChange={(e) =>
                                         setData("name", e.target.value)
@@ -92,7 +106,7 @@ export default function Transportation() {
                                     id="location"
                                     type="text"
                                     placeholder="Location"
-                                    className="w-full"
+                                    className="w-full rounded-md border border-gray-400 "
                                     value={data.location}
                                     onChange={(e) =>
                                         setData("location", e.target.value)
@@ -101,19 +115,6 @@ export default function Transportation() {
                                 <InputError message={errors.location} />
                             </div>
 
-                            {/* the text area for discription */}
-                            <FloatLabel>
-                                <InputTextarea
-                                    id="description"
-                                    value={data.description}
-                                    onChange={(e) =>
-                                        setData("description", e.target.value)
-                                    }
-                                    rows={5}
-                                    cols={30}
-                                />
-                                <label htmlFor="description">Description</label>
-                            </FloatLabel>
                             <div className="mb-3">
                                 <label
                                     htmlFor="tazkira_no"
@@ -125,7 +126,7 @@ export default function Transportation() {
                                     id="tazkira_no"
                                     type="text"
                                     placeholder="Tazkira Number"
-                                    className="w-full"
+                                    className="w-full rounded-md border border-gray-400 "
                                     value={data.tazkira_no}
                                     onChange={(e) =>
                                         setData("tazkira_no", e.target.value)
@@ -144,7 +145,7 @@ export default function Transportation() {
                                     id="phone"
                                     type="text"
                                     placeholder="Phone Number"
-                                    className="w-full"
+                                    className="w-full rounded-md border border-gray-400 "
                                     value={data.phone}
                                     onChange={(e) =>
                                         setData("phone", e.target.value)
@@ -157,15 +158,32 @@ export default function Transportation() {
                                     htmlFor="passport"
                                     className="block text-gray-700 font-medium mb-2"
                                 >
-                                    Licience Image
+                                    Licence Image
                                 </label>
-                                <input
-                                    type="file"
-                                    id="passport"
-                                    className="w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    name="passport"
-                                    onChange={handleFileUpload}
-                                />
+                                <div className="mb-3">
+                                    <InputText
+                                        type="file"
+                                        id="passport"
+                                        className="w-full rounded-md border border-gray-400 "
+                                        name="passport"
+                                        onChange={(e) =>
+                                            setData(
+                                                "passport",
+                                                e.target.files[0]
+                                            )
+                                        }
+                                    />
+                                    <InputError message={errors.passport} />
+                                    {passportImage && (
+                                        <div className="mt-3">
+                                            <img
+                                                src={setPassportImage}
+                                                alt="Passport Preview"
+                                                className="max-w-full h-auto rounded-md border border-gray-300 shadow-sm"
+                                            />
+                                        </div>
+                                    )}
+                                </div>
                                 {errors.passport && (
                                     <p className="text-red-600 text-sm mt-1">
                                         {errors.passport}
@@ -192,7 +210,7 @@ export default function Transportation() {
                                 <InputText
                                     type="file"
                                     id="image"
-                                    className="w-full"
+                                    className="w-full rounded-md border border-gray-400 "
                                     name="image"
                                     onChange={(e) =>
                                         setData("image", e.target.files[0])
@@ -209,6 +227,21 @@ export default function Transportation() {
                                     </div>
                                 )}
                             </div>
+                            {/* the text area for discription */}
+                            <FloatLabel className="w-full md:w-1/2">
+                                
+                                <InputTextarea
+                                    id="description"
+                                    value={data.description}
+                                    className="md:w-1/2"
+                                    onChange={(e) =>
+                                        setData("description", e.target.value)
+                                    }
+                                    rows={5}
+                                    cols={25}
+                                />
+                                <label htmlFor="description">Description</label>
+                            </FloatLabel>
 
                             <div
                                 style={{
@@ -219,7 +252,7 @@ export default function Transportation() {
                             >
                                 <Button
                                     label="Submit"
-                                    className="p-button"
+                                    className="p-button h-8"
                                     disabled={processing}
                                 />
                             </div>
