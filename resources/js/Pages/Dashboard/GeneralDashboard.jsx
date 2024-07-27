b1ca96ec182961b8b3c021c986b52a3af163d8d2
Najibullah Jafari <najib2020202020@gmail.com>
2024-07-27 21:10:13 +0430

    Add style

--- resources/js/Pages/Dashboard/GeneralDashboard.jsx
+++ resources/js/Pages/Dashboard/GeneralDashboard.jsx
@@ -1,8 +1,12 @@
-import { usePage } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 import TransportDashboard from "./TransportDashboard";
 import SuperAdminDashboard from "./SuperAdminDashboard";
 import HotelDashboard from "./HotelDashboard";
 import Layout from "@/Layouts/layout/layout";
+import SightSeeing from "../SightSeeing";
+import Navbar from "../Navbar";
+import Vission from "../Vission";
+import Footer from "../Footer";
 
 const GeneralDashboard = () => {
     const { user } = usePage().props;
@@ -12,6 +16,16 @@ const GeneralDashboard = () => {
 
     return (
         <>
+            {userRole === "user" && (
+                <>
+                    <Navbar />
+                    <div className="container card justify-center">
+                        <div>welcome {user.name}</div>
+                    </div>
+                    <Vission />
+                    <Footer />
+                </>
+            )}
             {userRole === "hotel-admin" && <HotelDashboard />}
             {userRole === "super-admin" && <SuperAdminDashboard />}
             {userRole === "transport-admin" && <TransportDashboard />}
